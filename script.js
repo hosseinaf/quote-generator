28cade7ab35a4d64b438ab3531cfe8f5e6797493
Hossein <hossein.aflaki70@gmail.com>
2023-08-02 14:08:45 +0330

    loader is done

--- script.js
+++ script.js
@@ -1,57 +1,82 @@
-// let apiQuote;
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// -----> If you're using the localQuotes array you can remove apiQuotes variable
 
-// function randomQuote(){
-//   const random=apiQuote[Math.floor(Math.random()*apiQuote.length)]
-//   console.log(random)
-// }
-// async function getQuote(){
-//   const apiUrl="https://type.fit/api/quotes"
-//   try{
-//     const response=await fetch(apiUrl);
-//     apiQuote=await response.json()
-//     randomQuote()
-//    // console.log(apiQuote)
-//   }
- 
-//   catch(error){
-//  console.log(error)
-//   }
-// }
+let apiQuotes = [];
 
-
-// getQuote()
-
-
-const quoteText=document.getElementById('quote')
-const authorText=document.getElementById('author')
-
-
-function randomQuote(){
-  const randomQuote=localQuotes[Math.floor(Math.random()*localQuotes.length)]
- // console.log(random)
-
- //if autor is null return undefine
- if(!randomQuote.author){
-  authorText.textContent=('Unknown')
- }
- else{
-  authorText.textContent=(randomQuote.author)
- }
-
- //if quote text is more than 50
- if(randomQuote.text.length>60){
-  quoteText.classList.add('long-quote')
- }
- else{
-  quoteText.classList.remove('long-quote')
- }
-
-
- 
-    quoteText.textContent=(randomQuote.text)
-  
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
-randomQuote()
+// Show New Quote
+function newQuote() {
+  loading();
+
+  // -----> Only use one of the following const quote statements
+
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  // Pick a random quote from localQuotes array
+  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// -----> If you're using the localQuotes array you can remove getQuotes() function 
+
+// Get Quotes From API 
+async function getQuotes() {
+  loading();
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
+
+// -----> If using API, run getQuotes(), if not run newQuote() instead
+// newQuote();
